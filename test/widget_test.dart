aad9d42f453f6d1e3ef00ec40f5d4f27a611511f
2dam3 <2dam3@MININT-EPL6B0M.H012108.NET>
2020-11-12 08:14:49 +0100

    actualizado

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:trabajo_evaluacion1/main.dart';
+import 'package:trabajo_evaluacion1/widgets/principal.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
